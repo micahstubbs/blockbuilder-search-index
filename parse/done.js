863927550f42c9899f4041a46a6bcc89cab3de8f
Micah Stubbs <micah.stubbs@gmail.com>
2018-08-26 00:22:42 -0700

    abstract out done

---
+++ parse/done.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+
+const done = function(parentProps, err) {
+  const {
+    missing,
+    apiHash,
+    colorHash,
+    allBlocks,
+    minBlocks,
+    apiBlocks,
+    colorBlocks,
+    colorBlocksMin,
+    fileBlocks,
+    libHash,
+    moduleHash
+  } = parentProps
+  console.log('done') //, apiHash
+  console.log(`skipped ${missing} missing files`)
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/apis.json',
+    JSON.stringify(apiHash)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/colors.json',
+    JSON.stringify(colorHash)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/blocks.json',
+    JSON.stringify(allBlocks)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/blocks-min.json',
+    JSON.stringify(minBlocks)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/blocks-api.json',
+    JSON.stringify(apiBlocks)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/blocks-colors.json',
+    JSON.stringify(colorBlocks)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/blocks-colors-min.json',
+    JSON.stringify(colorBlocksMin)
+  )
+  fs.writeFileSync(
+    __dirname + '/../data/parsed/files-blocks.json',
+    JSON.stringify(fileBlocks)
+  )
+
+  let libcsv = 'url,count\n'
+  Object.keys(libHash).forEach(
+    lib => (libcsv += lib + ',' + libHash[lib] + '\n')
+  )
+  fs.writeFileSync(__dirname + '/../data/parsed/libs.csv', libcsv)
+
+  let modulescsv = 'module,count\n'
+  Object.keys(moduleHash).forEach(
+    module => (modulescsv += module + ',' + moduleHash[module] + '\n')
+  )
+  fs.writeFileSync(__dirname + '/../data/parsed/modules.csv', modulescsv)
+
+  if (err) {
+    console.log('err', err)
+  }
+  console.log(`wrote ${apiBlocks.length} API blocks`)
+  console.log(`wrote ${colorBlocks.length} Color blocks`)
+  console.log(`wrote ${fileBlocks.length} Files blocks`)
+  return console.log(`wrote ${allBlocks.length} total blocks`)
+}
+
+module.exports = done
--- parse/index.js
+++ parse/index.js
@@ -15,6 +15,7 @@ const parseColors = require('./parse-colors.js')
 const parseLibs = require('./parse-libs.js')
 const parseD3Version = require('./parse-d3-version.js')
 const parseD3Modules = require('./parse-d3-modules.js')
+const done = require('./done.js')
 
 const allBlocks = []
 
@@ -41,63 +42,6 @@ const moduleHash = {}
 
 // number of missing files
 const missing = 0
-
-const done = function(err) {
-  console.log('done') //, apiHash
-  console.log(`skipped ${missing} missing files`)
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/apis.json',
-    JSON.stringify(apiHash)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/colors.json',
-    JSON.stringify(colorHash)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/blocks.json',
-    JSON.stringify(allBlocks)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/blocks-min.json',
-    JSON.stringify(minBlocks)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/blocks-api.json',
-    JSON.stringify(apiBlocks)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/blocks-colors.json',
-    JSON.stringify(colorBlocks)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/blocks-colors-min.json',
-    JSON.stringify(colorBlocksMin)
-  )
-  fs.writeFileSync(
-    __dirname + '/../data/parsed/files-blocks.json',
-    JSON.stringify(fileBlocks)
-  )
-
-  let libcsv = 'url,count\n'
-  Object.keys(libHash).forEach(
-    lib => (libcsv += lib + ',' + libHash[lib] + '\n')
-  )
-  fs.writeFileSync(__dirname + '/../data/parsed/libs.csv', libcsv)
-
-  let modulescsv = 'module,count\n'
-  Object.keys(moduleHash).forEach(
-    module => (modulescsv += module + ',' + moduleHash[module] + '\n')
-  )
-  fs.writeFileSync(__dirname + '/../data/parsed/modules.csv', modulescsv)
-
-  if (err) {
-    console.log('err', err)
-  }
-  console.log(`wrote ${apiBlocks.length} API blocks`)
-  console.log(`wrote ${colorBlocks.length} Color blocks`)
-  console.log(`wrote ${fileBlocks.length} Files blocks`)
-  return console.log(`wrote ${allBlocks.length} total blocks`)
-}
 
 // read in the list of gist metadata
 let gistMeta = JSON.parse(
@@ -215,5 +159,22 @@ module.exports = {
 }
 
 if (require.main === module) {
-  async.eachLimit(gistMeta, 100, gistParser, done)
+  async.eachLimit(
+    gistMeta,
+    100,
+    gistParser,
+    done.bind(null, {
+      missing,
+      apiHash,
+      colorHash,
+      allBlocks,
+      minBlocks,
+      apiBlocks,
+      colorBlocks,
+      colorBlocksMin,
+      fileBlocks,
+      libHash,
+      moduleHash
+    })
+  )
 }
