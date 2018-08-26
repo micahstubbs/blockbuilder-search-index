640ae971bc5bf6653905afef1d33bfe167030c53
Micah Stubbs <micah.stubbs@gmail.com>
2018-08-26 00:36:12 -0700

    abstract out gist-parser

--- parse/index.js
+++ parse/index.js
@@ -2,19 +2,14 @@ const fs = require('fs')
 const d3 = require('d3')
 const async = require('async')
 const request = require('request')
-const path = require('path')
 
-const pruneMin = require('./prune-min.js')
-const pruneApi = require('./prune-api.js')
-const pruneColors = require('./prune-colors.js')
-const pruneColorsMin = require('./prune-colors-min.js')
-const pruneFiles = require('./prune-files.js')
 const parseApi = require('./parse-api.js')
-const colorScales = require('./color-scales.js')
 const parseColors = require('./parse-colors.js')
-const parseLibs = require('./parse-libs.js')
+const colorScales = require('./color-scales.js')
 const parseD3Version = require('./parse-d3-version.js')
 const parseD3Modules = require('./parse-d3-modules.js')
+
+const gistParser = require('./gist-parser.js')
 const done = require('./done.js')
 
 const allBlocks = []
@@ -49,106 +44,10 @@ let gistMeta = JSON.parse(
 )
 
 // make gistMeta smaller for faster testing
-gistMeta = gistMeta.slice(0, 101)
+gistMeta = gistMeta.slice(0, 500)
 console.log(gistMeta.length)
 
 let i = 0
-const gistParser = function(gist, gistCb) {
-  //console.log "NOT RETURNING", gist.id, singleId
-  i++
-  console.log(i, gist.id)
-  const fileNames = Object.keys(gist.files)
-  // per-gist cache of api functions that we build up in place
-  const gapiHash = {}
-  const glibHash = {}
-  const gmoduleHash = {}
-  const gcolorHash = {}
-  const folder = __dirname + '/' + '../data/gists-files/' + gist.id
-  fs.mkdir(folder, function() {})
-
-  // we make a simplified data object for each file
-  const filepruned = pruneFiles(gist)
-  fileBlocks = fileBlocks.concat(filepruned)
-
-  return async.each(
-    fileNames,
-    function(fileName, fileCb) {
-      const ext = path.extname(fileName)
-      if (
-        [
-          '.html',
-          '.js',
-          '.coffee',
-          '.md',
-          '.json',
-          '.csv',
-          '.tsv',
-          '.css'
-        ].includes(ext)
-      ) {
-        const file = folder + '/' + fileName
-        return fs.readFile(file, function(err, data) {
-          let numColors
-          if (!data) {
-            return fileCb()
-          }
-          const contents = data.toString()
-          if (fileName === 'index.html') {
-            // TODO copy glibHash -> libHash etc for each of these
-            const numLibs = parseLibs(contents, gist, glibHash)
-            const version = parseD3Version(contents)
-            const modules = parseD3Modules(contents, gmoduleHash)
-            gist.d3version = version
-          }
-          if (['.html', '.js', '.coffee'].includes(ext)) {
-            // TODO copy gapiHash -> apiHash etc for each of these
-            const numApis = parseApi(contents, gist, gapiHash)
-            numColors = parseColors(contents, gist, gcolorHash)
-            colorScales(gapiHash, gcolorHash)
-            //console.log gist.id, fileName, numApis, numColors
-            return fileCb()
-          } else if (['.tsv', '.csv'].includes(ext)) {
-            // pull out # of rows and # of columns
-            return fileCb()
-          } else if (['.css'].includes(ext)) {
-            numColors = parseColors(contents, gist, gcolorHash)
-            //console.log gist.id, fileName, 0, numColors
-            return fileCb()
-          } else {
-            //console.log gist.id, fileName
-            return fileCb()
-          }
-        })
-      } else {
-        return fileCb()
-      }
-    },
-    function() {
-      if (Object.keys(gapiHash).length > 0) {
-        gist.api = gapiHash
-        apiBlocks.push(pruneApi(gist))
-      }
-      if (Object.keys(gmoduleHash).length > 0) {
-        gist.d3modules = gmoduleHash
-      }
-      if (Object.keys(gcolorHash).length > 0) {
-        gist.colors = gcolorHash
-        colorBlocks.push(pruneColors(gist))
-        colorBlocksMin.push(pruneColorsMin(gist))
-      }
-      // if Object.keys(glibHash).length > 0
-      //   gist.libs = glibHash
-
-      //delete gist.files
-      if (gist.files['thumbnail.png']) {
-        gist.thumbnail = gist.files['thumbnail.png'].raw_url
-      }
-      allBlocks.push(gist)
-      minBlocks.push(pruneMin(gist))
-      return gistCb()
-    }
-  )
-}
 
 module.exports = {
   api: parseApi,
@@ -162,7 +61,14 @@ if (require.main === module) {
   async.eachLimit(
     gistMeta,
     100,
-    gistParser,
+    gistParser.bind(null, {
+      fileBlocks,
+      allBlocks,
+      minBlocks,
+      colorBlocks,
+      colorBlocksMin,
+      apiBlocks
+    }),
     done.bind(null, {
       missing,
       apiHash,
