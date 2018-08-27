cc38e49d1fb5f25e1ca50ee908734e734cad89b2
Micah Stubbs <micah.stubbs@gmail.com>
2018-08-26 19:04:34 -0700

    improve filepath logging in writeJson

--- parse/write-json.js
+++ parse/write-json.js
@@ -4,12 +4,13 @@ const writeJson = function({ filePath, data, min, message }) {
   // pretty print with 2 spaces by default
   let space = 2
   let fullPath = `${__dirname}${filePath}`
+  const trimmedPath = filePath.replace('/../', '')
   if (min) space = null
   let writeMessage = ''
   if (Array.isArray(data)) {
-    writeMessage = `wrote ${data.length} ${message} to ${fullPath}`
+    writeMessage = `wrote ${data.length} ${message} to ${trimmedPath}`
   } else {
-    writeMessage = `wrote ${message} to ${fullPath}`
+    writeMessage = `wrote ${message} to ${trimmedPath}`
   }
 
   try {
