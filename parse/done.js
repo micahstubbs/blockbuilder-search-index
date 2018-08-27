e049e1c3efc7c7ad7521caaf3637abd1dde2b686
Micah Stubbs <micah.stubbs@gmail.com>
2018-08-26 19:23:29 -0700

    pad terminal output for file paths

--- parse/done.js
+++ parse/done.js
@@ -24,52 +24,82 @@ const done = function(parentProps, error) {
   //
   // write out the metadata we just parsed
   //
-  writeJson({
-    data: apiHash,
-    message: 'd3 API functions',
-    filePath: '/../data/parsed/apis.json'
-  })
-  writeJson({
-    data: colorHash,
-    message: 'colors',
-    filePath: '/../data/parsed/colors.json'
-  })
-  writeJson({
-    data: allBlocks,
-    message: 'block metadata entries',
-    filePath: '/../data/parsed/blocks.json'
-  })
-  writeJson({
-    min: true,
-    data: minBlocks,
-    message: 'minified block metadata entries',
-    filePath: '/../data/parsed/blocks-min.json'
-  })
-  writeJson({
-    data: apiBlocks,
-    message: 'd3 API functions entries',
-    filePath: '/../data/parsed/blocks-api.json'
-  })
-  writeJson({
-    data: colorBlocks,
-    message: 'colors entries',
-    filePath: '/../data/parsed/blocks-colors.json'
-  })
-  writeJson({
-    min: true,
-    data: colorBlocksMin,
-    message: 'minified colors entries',
-    filePath: '/../data/parsed/blocks-colors-min.json'
-  })
-  writeJson({
-    data: fileBlocks,
-    message: 'file metadata entries',
-    filePath: '/../data/parsed/files-blocks.json'
-  })
-  writeJson({
-    data: scriptTags,
-    message: 'script tags',
-    filePath: '/../data/parsed/script-tags.json'
+  const filesToWrite = [
+    {
+      data: apiHash,
+      message: 'd3 API functions',
+      filePath: '/../data/parsed/apis.json'
+    },
+    {
+      data: colorHash,
+      message: 'colors',
+      filePath: '/../data/parsed/colors.json'
+    },
+    {
+      data: allBlocks,
+      message: 'block metadata entries',
+      filePath: '/../data/parsed/blocks.json'
+    },
+    {
+      min: true,
+      data: minBlocks,
+      message: 'minified block metadata entries',
+      filePath: '/../data/parsed/blocks-min.json'
+    },
+    {
+      data: apiBlocks,
+      message: 'd3 API functions entries',
+      filePath: '/../data/parsed/blocks-api.json'
+    },
+    {
+      data: colorBlocks,
+      message: 'colors entries',
+      filePath: '/../data/parsed/blocks-colors.json'
+    },
+    {
+      min: true,
+      data: colorBlocksMin,
+      message: 'minified colors entries',
+      filePath: '/../data/parsed/blocks-colors-min.json'
+    },
+    {
+      data: fileBlocks,
+      message: 'file metadata entries',
+      filePath: '/../data/parsed/files-blocks.json'
+    },
+    {
+      data: scriptTags,
+      message: 'script tags',
+      filePath: '/../data/parsed/script-tags.json'
+    }
+  ]
+  const maxMessageLength = Math.max(
+    ...filesToWrite.map(file => file.message.length)
+  )
+  const maxDataLength = Math.max(
+    ...filesToWrite.map(file => {
+      if (Array.isArray(file.data)) return String(file.data.length).length + 1
+      return 0
+    })
+  )
+  const fixedLength = 10
+  const maxLength = fixedLength + maxDataLength + maxMessageLength
+  console.log('fixedLength', fixedLength)
+  console.log('maxDataLength', maxDataLength)
+  console.log('maxMessageLength', maxMessageLength)
+  console.log('maxLength', maxLength)
+
+  filesToWrite.forEach(file => {
+    let dataLength = 0
+    if (Array.isArray(file.data))
+      dataLength = String(file.data.length).length + 1
+    const messageLength = file.message.length
+    const sumLength = fixedLength + dataLength + messageLength
+    if (sumLength < maxLength) {
+      // pad the message length for nice terminal output
+      file.message = `${file.message}${' '.repeat(maxLength - sumLength)}`
+    }
+    writeJson(file)
   })
 
   let libcsv = 'url,count\n'
