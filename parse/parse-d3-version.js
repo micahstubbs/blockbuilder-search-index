4e4c5740a963f408b43fb13e59e89b1618f732cb
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-11 23:11:33 -0700

    parse out d3 v5

--- parse/parse-d3-version.js
+++ parse/parse-d3-version.js
@@ -6,16 +6,18 @@ const parseD3Version = function({ code, scriptTags, scriptTagHash }) {
 
   let version = 'NA'
   scripts.forEach(function(script) {
-    if (script.indexOf('d3.v4') >= 0) {
+    if (script.indexOf('d3.v5') > -1) {
+      version = 'v5'
+    } else if (script.indexOf('d3.v4') > -1) {
       version = 'v4'
-    } else if (script.indexOf('d3/3.') >= 0 || script.indexOf('d3.v3') >= 0) {
+    } else if (script.indexOf('d3/3.') > -1 || script.indexOf('d3.v3') > -1) {
       version = 'v3'
     }
-    if (script.indexOf('d3.v2') >= 0) {
+    if (script.indexOf('d3.v2') > -1) {
       return (version = 'v2')
     } else if (
-      script.indexOf('d3.js') >= 0 ||
-      script.indexOf('d3.min.js') >= 0
+      script.indexOf('d3.js') > -1 ||
+      script.indexOf('d3.min.js') > -1
     ) {
       // we know this is some sort of d3 but not which version
       if (version === 'NA') {
